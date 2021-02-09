27cb390c9a61f297b387f7031333753f41e35a10
hari <harikrishkk@gmail.com>
2021-02-09 01:05:40 -0500

    Section 2 Part 14 -test passes

--- src/app/_services/adapters/http-adapter/http-adapter.service.spec.ts
+++ src/app/_services/adapters/http-adapter/http-adapter.service.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed, fakeAsync } from '@angular/core/testing';
+import { Spy, createSpyFromClass } from 'jasmine-auto-spies';
 import { HttpAdapterService } from './http-adapter.service';
+import serverMock from 'xhr-mock';
 
 describe('HttpAdapterService', () => {
-  let service: HttpAdapterService;
+  let serviceUnderTest: HttpAdapterService;
+  let actualResult: any;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpAdapterService);
+  Given(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [HttpAdapterService]
+    });
+    serviceUnderTest = TestBed.inject(HttpAdapterService);
+    actualResult = undefined;
+    serverMock.setup();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    serverMock.teardown();
+  });
+
+  describe('Method patch', () => {
+    let fakeUrlArg: string;
+    let fakeBodyArg: any;
+    let expectedReturnedResult: any;
+    let actualBodySent: any;
+    Given(() => {
+      fakeUrlArg = '';
+      fakeBodyArg = {
+        fake: 'body'
+      };
+
+      expectedReturnedResult = {
+        fake: 'result'
+      };
+
+      serverMock.patch(fakeUrlArg, (req, res) => {
+        actualBodySent = JSON.parse(req.body());
+        return res.status(200).body(JSON.stringify(expectedReturnedResult));
+      });
+    });
+    When(
+      fakeAsync(async () => {
+        actualResult = await serviceUnderTest.patch(fakeUrlArg, fakeBodyArg);
+      })
+    );
+
+    Then(() => {
+      expect(actualResult).toEqual(expectedReturnedResult);
+    });
   });
 });
--- src/app/_services/adapters/http-adapter/http-adapter.service.ts
+++ src/app/_services/adapters/http-adapter/http-adapter.service.ts
@@ -1,12 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpAdapterService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  patch(url: string, body: any): Promise<any> {
-    throw new Error('Method not implemented');
+  get() {}
+
+  patch<T>(url: string, body: any): Promise<T> {
+    return this.httpClient.patch<T>(url, body).toPromise();
   }
 }
